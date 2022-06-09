dff51afe35c3fd1e43cd9f1d5c9308141f245714
Declan Davis <dexthefish@gmail.com>
2022-06-08 21:53:31 -0400

    verified that fetcher will overwrite an existing file if asked to. Added clause in fetcher to stop running when encountering an invalid url.

--- fetcher.js
+++ fetcher.js
@@ -4,7 +4,10 @@ const fs = require('fs');
 
 const request = require('request');
 request(url, function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
+  if (error) {
+    console.error('error:', error); // Print the error if one occurred
+    return;
+  }
   console.log('statusCode:', response && response.statusCode);
   fs.writeFile(filePath, body, err => {
     if (err) {
